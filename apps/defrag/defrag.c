9d40f30940905707421e8ab7c631e915a53b84a3
Kuei Sun <kuei.sun@mail.utoronto.ca>
2021-06-10 15:51:06 -0400

    defrag tool fully done using newest evfs interface and full scaffolding of userspace API

--- apps/defrag/defrag.c
+++ apps/defrag/defrag.c
@@ -5,9 +5,10 @@
  *
  */
 
-#include <evfs.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <evfs.h>
 
 int usage(char * prog)
 {
@@ -18,10 +19,33 @@ int usage(char * prog)
     return 1;
 }
 
+// 1 for yes, 0 for no
+int should_defragment(evfs_t * evfs, struct evfs_super_block * sb, long ino_nr)
+{
+    // for now, always defragment
+    (void)evfs;
+    (void)sb;
+    (void)ino_nr;
+    return 1;
+}
+
+#define CEILING(x,y) (((x) + (y) - 1) / (y))
+
 int defragment(evfs_t * evfs, struct evfs_super_block * sb, long ino_nr)
 {
     struct evfs_inode inode;
+    char * data = NULL;
     int ret;
+    u64 poff, loff = 0;
+    u64 nr_blocks;
+    struct evfs_imap * imap = NULL;
+    struct evfs_timeval mtime;  // to avoid race condition
+    u64 extent_size;
+    u64 byte_size;
+
+    if (!should_defragment(evfs, sb, ino_nr)) {
+        return 0;
+    }
     
     inode.ino_nr = ino_nr;
     ret = inode_info(evfs, &inode);
@@ -29,11 +53,64 @@ int defragment(evfs_t * evfs, struct evfs_super_block * sb, long ino_nr)
     if (!ret) {
         return ret;
     }
+          
+    // calculate how many blocks need to be allocated
+    imap = imap_new(evfs);
+    nr_blocks = CEILING(inode.bytesize, sb->block_size);       
+    extent_size = (nr_blocks > sb->max_extent_size) ? sb->max_extent_size : nr_blocks;
+    byte_size = extent_size * sb->block_size;   
     
-    /* refactor defragmentation logic here */
+    // set up buffer for copying data to new extents   
+    data = malloc(byte_size);
+    if (!data) {
+        ret = -ENOMEM;
+        goto fail;
+    }      
+          
+    // start allocating new contiguous extents. we have to deal with the
+    // possibility that the file system has a maximum contiguous extent limit
+    while (nr_blocks > 0) {
+        poff = extent_alloc(evfs, 0, extent_size, 0 /* no hint */);
+        if (!poff) {
+	        return -ENOSPC;	// error
+        }
+
+        ret = imap_append(imap, loff, poff, extent_size);
+        if (ret < 0) {
+            goto fail;
+        }   
+           
+        ret = inode_read(evfs, ino_nr, loff * sb->block_size, data, byte_size);
+        if (ret < 0) {
+            goto fail;
+        }
+        
+        ret = extent_write(evfs, poff, 0, data, byte_size);
+        if (ret < 0) {
+            goto fail;            
+        }
+        
+        // prepare for next iteration
+        nr_blocks -= extent_size;
+        loff += extent_size;
+        extent_size = (nr_blocks > sb->max_extent_size) ? sb->max_extent_size : nr_blocks;
+        byte_size = extent_size * sb->block_size; 
+    }
     
-    (void)sb;
-    return ret;    
+    ret = inode_map(evfs, ino_nr, imap);
+    if (ret < 0) {
+        goto fail;
+    }
+    
+    (void)mtime;
+    free(data);
+    imap_free(evfs, imap, 1 /* do not free extents on success */);
+    return 0;
+    
+fail:
+    free(data);
+    imap_free(evfs, imap, 0 /* free all extents upon error */);
+    return ret;
 }
 
 int defragment_all(evfs_t * evfs, struct evfs_super_block * sb)
--- apps/evfs/evfs.h
+++ apps/evfs/evfs.h
@@ -10,8 +10,11 @@
 
 typedef unsigned long long u64;
 typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
 typedef long long i64;
 typedef int i32;
+
 
 typedef struct {
     int fd;
@@ -32,13 +35,13 @@ struct evfs_super_block {
     u64 max_bytes;        /* max file size */
     u64 block_count;      /* total number of data blocks available */
     u64 root_ino;         /* root inode number */
+    u64 block_size;
 };
 
 struct evfs_inode_property {
-    int inlined;        // does inode have inlined data
-    int refcount;       // link count
-    long blockcount;    // number of blocks used
-    long bytesize;      // size of inode, in bytes
+    u32 refcount;       // link count
+    u64 blockcount;     // number of blocks used
+    u64 inlined_bytes;  // number of bytes inlined in the file system
 };
 
 struct evfs_timeval {
@@ -47,22 +50,37 @@ struct evfs_timeval {
 };
 
 struct evfs_inode {
-    unsigned long ino_nr;
+    u64 ino_nr;
 
     struct evfs_timeval atime;
     struct evfs_timeval ctime;
     struct evfs_timeval mtime;
     struct evfs_timeval otime;
 
-    int /* kuid_t */ uid;
-    int /* kgid_t */ gid;
-    unsigned short /* umode_t */ mode;
-    unsigned int flags;
+    u32 /* kuid_t */ uid;
+    u32 /* kgid_t */ gid;
+    u16 /* umode_t */ mode;
+    u32 flags;
+    u64 bytesize;
 
     union {
         const struct evfs_inode_property prop;  /* users read this */
         struct evfs_inode_property _prop;       /* kernel modify this */
     };
+};
+
+// mapping entry
+struct evfs_imentry {
+    u32 inlined;    // this map entry is inlined (e.g., tail-packed)
+    u64 log_addr;
+    u64 phy_addr;
+    u64 len;
+};
+
+struct evfs_imap {
+    u32 count;      // number of active entries
+    u32 capacity;   // number of entries that fits (i.e. preallocated)
+    struct evfs_imentry entry[];
 };
 
 // basic open/close for evfs device
@@ -75,11 +93,24 @@ evfs_iter_t * extent_iter(evfs_t * evfs, int flags);    // free space
 u64 inode_next(evfs_iter_t * it);
 void iter_end(evfs_iter_t * it);
 
+// extent
+int extent_alloc(evfs_t * evfs, u64 pa, u64 len, int flags);
+int extent_free(evfs_t * evfs, u64 pa);
+int extent_write(evfs_t * evfs, u64 pa, u64 off, char * buf, u64 len);
+
 // super block
 int super_info(evfs_t * evfs, struct evfs_super_block * sb);
 
 // inode
 int inode_info(evfs_t * evfs, struct evfs_inode * inode);
+int inode_map(evfs_t * evfs, u64 ino_nr, struct evfs_imap * imap);
+int inode_read(evfs_t * evfs, u64 ino_nr, u64 off, char * buf, u64 len);
+
+// inode mapping
+struct evfs_imap * imap_new(evfs_t * evfs);
+struct evfs_imap * imap_info(evfs_t * evfs, u64 ino_nr);
+int imap_append(struct evfs_imap * imap, u64 la, u64 pa, u64 len);
+void imap_free(evfs_t * evfs, struct evfs_imap * imap, int nofree);
 
 #endif
 
